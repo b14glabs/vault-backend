df18e536ced429d4d3e9d3204f506e9bedc20805
nguyenHoang32 <nguyenhuuhoang2k15@gmail.com>
2025-01-10 10:20:11 +0700

    reduce docs save exchangeRate save in db

--- src/listenExchangeRate.ts
+++ src/listenExchangeRate.ts
@@ -6,13 +6,18 @@ import { createExchangeRates } from "./services/exchangeRate.service";
 import coreVaultAbi from "./abi/core-vault.json";
 
 export async function listenExchangeRate() {
+  let isSave = false;
   try {
+    const current = new Date();
+    const hourUtc = current.getUTCHours();
+    if (hourUtc !== 0) {
+      return;
+    }
     const coreVaultContract = new Contract(
       contractAddresses.coreVault,
       coreVaultAbi,
       jsonRpc
     ) as unknown as CoreVault;
-
 
     const [blockNumber, exchangeRate] = await Promise.all([
       jsonRpc.getBlockNumber(),
@@ -24,11 +29,15 @@ export async function listenExchangeRate() {
       date: new Date(),
       exchangeRate: exchangeRate.toString(),
     });
+    isSave = true;
   } catch (error) {
     log("listenExchangeRate error :" + error);
   } finally {
-    setTimeout(() => {
-      listenExchangeRate();
-    }, 1000 * 60 * 30);
+    setTimeout(
+      () => {
+        listenExchangeRate();
+      },
+      isSave ? 1000 * 60 * 60 * 12 : 1000 * 60 * 7
+    );
   }
 }
