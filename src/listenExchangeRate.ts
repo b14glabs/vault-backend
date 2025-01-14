e14dc695344fe644154a3861ed6c83697710fd75
nguyenHoang32 <nguyenhuuhoang2k15@gmail.com>
2025-01-14 11:13:00 +0700

    optimize listenExchangeRate delay time

--- src/listenExchangeRate.ts
+++ src/listenExchangeRate.ts
@@ -2,28 +2,43 @@ import { log } from "./util";
 import { contractAddresses, jsonRpc, RPC_URL } from "./consts";
 import { Contract, parseEther } from "ethers";
 import { CoreVault } from "./types/dualCore";
-import { createExchangeRates } from "./services/exchangeRate.service";
+import {
+  createExchangeRates,
+  findLastExchangeRate,
+} from "./services/exchangeRate.service";
 import coreVaultAbi from "./abi/core-vault.json";
 
 export async function listenExchangeRate() {
+  let delayTimeOut = 60 * 1000 * 10; // 10m
   try {
     const current = new Date();
     const hourUtc = current.getUTCHours();
-    const minuteUtc = current.getUTCMinutes()
-    if (hourUtc !== 0 || minuteUtc > 20) {
+    const minuteUtc = current.getUTCMinutes();
+    // Prevent trigger many time 
+    if (hourUtc !== 0 || minuteUtc > 30) {
       return;
     }
-   
+
     const coreVaultContract = new Contract(
       contractAddresses.coreVault,
       coreVaultAbi,
       jsonRpc
     ) as unknown as CoreVault;
 
-    const [blockNumber, exchangeRate] = await Promise.all([
+    const [blockNumber, exchangeRate, lastExchangeRate] = await Promise.all([
       jsonRpc.getBlockNumber(),
       coreVaultContract.exchangeCore.staticCall(parseEther("1")),
+      findLastExchangeRate(),
     ]);
+    
+    if (exchangeRate == BigInt(lastExchangeRate.exchangeRate)) {
+      return
+    }
+    
+    const tomorrow = new Date();
+    tomorrow.setUTCDate(current.getUTCDate() + 1);
+    tomorrow.setUTCHours(0, 10, 0, 0);
+    delayTimeOut = tomorrow.getTime() - current.getTime()
 
     await createExchangeRates({
       blockNumber: Number(blockNumber),
@@ -33,11 +48,8 @@ export async function listenExchangeRate() {
   } catch (error) {
     log("listenExchangeRate error :" + error);
   } finally {
-    setTimeout(
-      () => {
-        listenExchangeRate();
-      },
-      1000 * 60 * 7
-    );
+    setTimeout(() => {
+      listenExchangeRate();
+    }, delayTimeOut);
   }
 }
