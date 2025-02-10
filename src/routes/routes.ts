a1eb7fca82638eda5b445886994f7405f44b47d8
chn24 <huynghia24082002@gmail.com>
2025-02-10 10:09:48 +0700

    fix: remove ;

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -12,16 +12,16 @@ import {
 
 const router = Router();
 
-router.get("/history", getEvents);;
-router.get("/user-history/:address", getEventsByUser);;
+router.get("/history", getEvents);
+router.get("/user-history/:address", getEventsByUser);
 router.get("/latest-exchange-rate", getLatestExchangeRate);
-router.get("/claimed-reward", getClaimedReward);;
+router.get("/claimed-reward", getClaimedReward);
 
 router.get("/daily-apy", getDailyApy);
 router.get("/apy-chart", getApyChart);
 // Used for marketplace stats. Do not delete
 router.get("/stats", getStats);
 
-router.get("/check-staked/:delegator", checkUserStaked);;
+router.get("/check-staked/:delegator", checkUserStaked);
 
 export default router;
